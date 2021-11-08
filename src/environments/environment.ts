a1dfb60d49b74f4fed83b4a46f92b124818c0cbc
MicroTot <32261713+MicroTot@users.noreply.github.com>
2021-11-08 15:03:01 +0300

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   APIurl: "https://pesapalscheduler2.herokuapp.com"
 };
 
